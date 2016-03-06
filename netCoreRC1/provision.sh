6eb54b3c59089faa23bf108f32c25077726e3487
geobarteam <gvd8@hotmail.com>
2016-03-06 14:19:58 +0100

    Update provision.sh

--- netCoreRC1/provision.sh
+++ netCoreRC1/provision.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get -y install git 
 sudo apt-get -y install ubuntu-desktop 
 sudo apt-get -y install virtualbox-guest-utils virtualbox-guest-x11  virtualbox-guest-dkms 
-sudo apt-get -y install nodejs npm
+sudo apt-get -y install nodejs-legacy
 sudo apt-get -y install build-essential openssl libssl-dev curl 
 
 #yomen
